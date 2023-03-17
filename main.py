7cb0908afa08a43f677f70a629e2705d480c9414
Janny <jan-louis.schneider@gmx.de>
2023-03-17 17:02:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+import sys
+import MainWindow
+import threading
+import time
+import random
+import MainWindow
+import Total
+import Residents
+
+
+# ----- Run this class to run the Program -----
+
+def runnn():
+    MainWindow.runMain()
+
+runnn()
